f93a18bbf665fac8124e878fbc8e0c186f4eb73e
michaelSandros <sandros@ceid.upatras.gr>
2016-03-24 01:22:16 +0200

    Added files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Imports
+from linear_threshold import *
+from myGraph import *
+from calculateNodes import *
+
+G,pos,labels,edgeList,n,seed = createGraph()
+#Call of linear_threshold - Random Euristic
+randomThres = linear_threshold(G,[seed],-4)
+#Call of linear_threshold - High Degree Euristic
+highDegree = highDegree(G,n)
+highDegree = linear_threshold(G,[seed],-4)
+
+print "Random Thresholds"
+calculateNodes(randomThres)
+print "High Degree Thresholds"
+calculateNodes(highDegree)
+graphDraw(G,pos,labels,edgeList)
