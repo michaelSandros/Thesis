dcd7fc07cc643ceecb40607c639e8a48c9f9143f
michaelSandros <sandros@ceid.upatras.gr>
2016-04-14 14:14:41 +0300

    Added files via upload

--- communities.py
+++ communities.py
@@ -58,7 +58,7 @@ def initialNodes(G,comms):
         for k in range(1,K+1):
             for m in range(1,M+1):
                 for l in range(0,len(comms[m-1])):
-                    #union
+                    #union I with the influential nodes
                     tempList = I + [comms[m-1][l]]
                     # edw mallon prepei na einai ypografima
                     union = linear_threshold(G,tempList,steps = -4)
@@ -74,13 +74,11 @@ def initialNodes(G,comms):
                     DRm = max(DRmList)
                 R[m][k] = max(R[m-1][k],R[M][k-1]+DRm)
                 if (R[m-1][k] >= R[M][k-1] + DRm):
-                    print("Hello")
                     s[m][k] = s[m-1][k]
                 else:
-                    print("Hello THere")
                     s[m][k] = m
                 DRmList = []
-            #community numbering begins from 0 
+            # community numbering begins from 0 
             j = s[M][k] - 1
             print(j)
             for x in range(0, len(comms[j])):
@@ -96,6 +94,7 @@ def initialNodes(G,comms):
             else:
                 m = max(maxList)
             index = [i for i, j in enumerate(maxList) if j == m]
+            # random in
             newIndex = random.choice(index)
             Ij[j].extend([comms[j][newIndex]])
             I.extend([comms[j][newIndex]])
